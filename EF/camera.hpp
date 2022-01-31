2e4c1e4fb98d61849d7afebcadf377418074cfeb
Dusan Fajler <dusanfajler@gmail.com>
2022-01-31 22:44:21 +0100

    14. camera stuff

--- EF/camera.hpp
+++ EF/camera.hpp
@@ -9,14 +9,19 @@ namespace ef {
 
     class Camera {
     public:
-        void setOrthographicProjection(
-            float left, float right, float top, float bottom, float near, float far);
-
+        void setOrthographicProjection(float left, float right, float top, float bottom, float near, float far);
         void setPerspectiveProjection(float fovy, float aspect, float near, float far);
 
-        const glm::mat4& getProjection() const { return projectionMatrix; }
+        void setViewDirection(glm::vec3 position, glm::vec3 direction, glm::vec3 up = glm::vec3{ 0.f, -1.f, 0.f });
+        void setViewTarget(glm::vec3 position, glm::vec3 target, glm::vec3 up = glm::vec3{ 0.f, -1.f, 0.f });
+        void setViewYXZ(glm::vec3 position, glm::vec3 rotation);
 
+
+        const glm::mat4& getProjection() const { return projectionMatrix; }
+        const glm::mat4& getView() const { return viewMatrix; }
     private:
         glm::mat4 projectionMatrix{ 1.f };
+        glm::mat4 viewMatrix{ 1.0 };
+
     };
 }  // namespace ef
--- EF/first_app.cpp
+++ EF/first_app.cpp
@@ -25,12 +25,15 @@ namespace ef {
         RenderSystem simpleRenderSystem{ device, renderer.getSwapChainRenderPass() };
         Camera camera{};
 
+        //camera.setViewDirection(glm::vec3(0.f), glm::vec3(0.5f, 0.f, 1.0f));
+        camera.setViewTarget(glm::vec3(-1.f, -2.f, -20.f), glm::vec3(0.0f, 0.f, 2.5f));
+
         while (!window.shouldClose()) {
             glfwPollEvents();
 
             float aspect = renderer.getAspectRatio();
             //camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
-            camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
+            camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 100.f);
             if (auto commandBuffer = renderer.beginFrame()) {
                 renderer.beginSwapChainRenderPass(commandBuffer);
                 simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
