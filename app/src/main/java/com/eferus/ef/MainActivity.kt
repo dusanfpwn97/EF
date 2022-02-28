6cb155203985e27b799f99eec3e453c356472a9b
Dusan Fajler <dusanfajler@gmail.com>
2022-02-28 22:54:04 +0100

    android setup wip

--- app/src/main/java/com/eferus/ef/MainActivity.kt
+++ app/src/main/java/com/eferus/ef/MainActivity.kt
@@ -16,14 +16,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        binding.sampleText.text = stringFromJNI2()
     }
 
     /**
      * A native method that is implemented by the 'ef' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    external fun stringFromJNI2(): String
 
     companion object {
         // Used to load the 'ef' library on application startup.
--- src/entry_point.h
+++ src/entry_point.h
@@ -1,7 +1,7 @@
 #pragma once
 
 //#ifdef EF_PLATFORM_WINDOWS
-#include <string>
+/*#include <string>
 extern ef::Application* ef::createApplication();
 
 int main(int argc, char** argv)
@@ -13,6 +13,17 @@ int main(int argc, char** argv)
 	app->run();
 
 	delete app;
-}
+}*/
 
 //#endif
+
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_eferus_ef_MainActivity_stringFromJNI2(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello 2 C++";
+    return env->NewStringUTF(hello.c_str());
+}
