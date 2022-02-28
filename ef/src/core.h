41d3a09db974a9a41a5931d2b974350dba79ecdf
Dusan Fajler <dusanfajler@gmail.com>
2022-03-01 00:29:04 +0100

    restructure to support android wip

---
+++ ef/src/core.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef EF_PLATFORM_WINDOWS
+	#ifdef EF_BUILD_DLL
+		#define EF_API __declspec(dllexport)
+	#else
+		#define EF_API __declspec(dllimport)
+	#endif
+#else
+	#ifdef EF_BUILD_DLL
+		#define EF_API __declspec(JNIEXPORT)
+	#else
+		#define EF_API __declspec(JNIEXPORT)
+	#endif
+#endif
