e43238ced4325367b8bc0d17f1b6dcd2ad866c0b
Dusan Fajler <dusanfajler@gmail.com>
2022-02-28 22:33:52 +0100

    Android

---
+++ android/ef/app/src/main/java/com/eferus/ef/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.eferus.ef
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.eferus.ef.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'ef' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'ef' library on application startup.
+        init {
+            System.loadLibrary("ef")
+        }
+    }
+}
