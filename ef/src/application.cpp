4df82e09f6580a3b8ac5d919b14775749c9b913a
Dusan Fajler <dusanfajler@gmail.com>
2022-03-02 20:11:31 +0100

    sss

--- ef/src/application.cpp
+++ ef/src/application.cpp
@@ -19,7 +19,25 @@ namespace ef
 
 	void Application::run()
 	{
-        
+		SDL_Window* window;
+		SDL_Init(SDL_INIT_VIDEO);
+		window = SDL_CreateWindow(
+			"An SDL2 window",                  // window title
+			SDL_WINDOWPOS_UNDEFINED,           // initial x position
+			SDL_WINDOWPOS_UNDEFINED,           // initial y position
+			640,                               // width, in pixels
+			480,                               // height, in pixels
+			SDL_WINDOW_OPENGL                  // flags - see below
+		);
+
+
+		// Check that the window was successfully created
+		if (window == NULL) {
+			// In the case that the window could not be made...
+			printf("Could not create window: %s\n", SDL_GetError());
+			return;
+		}
+
 		while (1)
 		{
 
--- ef/src/entry_point.h
+++ ef/src/entry_point.h
@@ -23,7 +23,7 @@ int main(int argc, char** argv)
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_eferus_ef_MainActivity_stringFromJNI2(
         JNIEnv* env,
-        jobject /* this */) {
+        jobject this ) {
     std::string hello = "Hello 2 C++";
     return env->NewStringUTF(hello.c_str());
 }*/
